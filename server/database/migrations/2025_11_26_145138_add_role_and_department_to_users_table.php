9acbd75a23704610c24908186c13318ab14d0270
kheirallah17 <kheirallahmawla19@gmail.com>
2025-11-26 16:58:14 +0200

    Correct users table migrations and remove duplicate migration

--- server/database/migrations/2025_11_26_145138_add_role_and_department_to_users_table.php
+++ server/database/migrations/2025_11_26_145138_add_role_and_department_to_users_table.php
@@ -6,31 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-       Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100);
-            $table->string('email', 150)->unique();
+        Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')->nullable();
             $table->foreignId('department_id')->nullable();
-            $table->string('password');
             $table->enum('status', ['active','inactive','suspended'])->default('active');
-            $table->timestamps(); // created_at & updated_at
-});
-
-
-
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role_id', 'department_id', 'status']);
+        });
     }
 };
