c3a64e3dd1d05c44ce19263961c1f694f6d0a8a4
Hikmat Audi <143126494+hikmataudi1@users.noreply.github.com>
2025-12-02 19:09:49 +0200

    Refactor App component to use React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import CitizenRegistration from './pages/CitizenRegistration';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Profile from './pages/Profile';
+import CertificateRequestsPage from './pages/CertificateRequestsPage';
+import NewRequest from './pages/newRequest'
+import Viewrequest from './pages/Viewrequest'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<CitizenRegistration />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/Request" element={<CertificateRequestsPage/>}/>
+        <Route path='/newRequest'element={<NewRequest/>}/>
+        <Route path='/Viewrequest/:id'element={<Viewrequest/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
