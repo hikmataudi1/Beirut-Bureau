25f7f3c7ede4395c6d1db898050c8d18df500ae2
yahyaabh <yehia.abu.alhassan.01@gmail.com>
2025-12-04 22:36:28 +0200

    role-id ----> role

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      protected $fillable = [
         'name',
         'email',
-        'role_id',
+        'role',
         'department_id',
         'password',
         'status',
--- server/database/migrations/2025_11_26_145138_add_role_and_department_to_users_table.php
+++ server/database/migrations/2025_11_26_145138_add_role_and_department_to_users_table.php
@@ -9,6 +9,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            
+            $table->enum('role', [
+                            'Admin',
+                            'Mayor',
+                            'Municipal Director',
+                            'Finance Officer',
+                            'Urban Planner',
+                            'Project Manager',
+                            'HR Manager',
+                            'Clerk',
+                            'Staff',
+                            'Citizen',
+                            'Resident'
+                        ])->nullable()->after('id');
+
             $table->foreignId('role_id')->nullable();
             $table->foreignId('department_id')->nullable();
             $table->enum('status', ['active','inactive','suspended'])->default('active');
